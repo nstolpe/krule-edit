027026ac880857f475b7d2e1c24f86b6d2c81dff
nils <slincire@gmail.com>
2015-06-17 11:38:49 -0400

    added some test code into the helper that will parse the document

--- app/helpers/parse-document.js
+++ app/helpers/parse-document.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 // use this to build the text fields and separators you'll need.
-export default Ember.Handlebars.makeBoundHelper(function(value,options) {
-	console.log(value);
-	return JSON.stringify(value);
+export default Ember.Handlebars.makeBoundHelper(function(content,options) {
+	function foo(content) {
+		console.log(content);
+	}
+	for (var i = 0, l = content.length; i < l; i++) {
+		if (Array.isArray(content))
+	}
+	foo('value');
+	return JSON.stringify(content);
 });
