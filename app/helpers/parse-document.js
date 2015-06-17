e31282c1c2c7a6ef0fd61c7cba46b6dd553631ed
nils <nils@gangshi>
2015-06-17 18:58:10 -0400

    got the various text nodes outputting to different textareas

--- app/helpers/parse-document.js
+++ app/helpers/parse-document.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 
 // use this to build the text fields and separators you'll need.
 export default Ember.Handlebars.makeBoundHelper(function(content,options) {
-	function foo(content) {
-		console.log(content);
+	var outputContainer = $('<div></div>');
+
+	function recursiveText(node) {
+		node.forEach(function(element, index, array) {
+			if(Array.isArray(element)) {
+				recursiveText(element);
+			} else {
+				outputContainer.append('<textarea>' + element + '</textarea>');
+			}
+		});
 	}
-	for (var i = 0, l = content.length; i < l; i++) {
-		if (Array.isArray(content))
-	}
-	foo('value');
-	return JSON.stringify(content);
+
+	recursiveText(content);
+
+	return new Ember.Handlebars.SafeString(outputContainer.html());
 });
